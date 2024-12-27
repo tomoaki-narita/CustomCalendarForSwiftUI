40ffb023ae6859f32a80ed4f375a41fdfc11d17a
tomoaki-narita <output.org@gmail.com>
2024-12-27 11:01:23 +0900

    Initial Commit

---
+++ CustomCalendarForSwiftUI/CustomCalendarForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCalendarForSwiftUIApp.swift
+//  CustomCalendarForSwiftUI
+//
+//  Created by output. on 2024/12/27.
+//
+
+import SwiftUI
+
+@main
+struct CustomCalendarForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
